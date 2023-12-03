43b5dde2515b02a899da6c9cabd28762bd16cf32
SRINIVAS BANOTH <145092139+srinu54321@users.noreply.github.com>
2023-12-03 20:43:07 +0530

    Add files via upload

---
+++ first_javascript.js
@@ -0,0 +1,17 @@
+// let a="harry";
+// let b=9;
+// console.log(a+b)
+
+// const a={
+//     name:"srinivas",
+//     age:34,
+//     college:"bv raju institute of technology,narsapur",
+//     village:"aurangabad thanda"
+// }
+// a['friend']='rama'
+// console.log(a)
+const dict={
+    name:"ramachandra",wife:"sitha matha",name_second:"krishna",wife_of_second_name:"16108"
+}
+console.log(dict)
+console.log(dict["name"])
