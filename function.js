5047fe18763ff7d4e0f437ab9d2b5027db927052
SRINIVAS BANOTH <145092139+srinu54321@users.noreply.github.com>
2023-12-11 21:48:22 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,11 @@
+function onepluse(x,y){
+    return 1+2*(x+y)
+}
+let a=1;
+let b=2;
+let c=3;
+console.log("the first answer is ",onepluse(a,b))
+console.log("the first answer is ",onepluse(b,c))
+console.log("the first answer is ",onepluse(c,a))
+
+
