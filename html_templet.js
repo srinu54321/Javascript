d5885f309eb995accd35f3809abc5fd1da23733e
SRINIVAS BANOTH <145092139+srinu54321@users.noreply.github.com>
2023-12-26 22:20:19 +0530

    Add files via upload

---
+++ html_templet.js
@@ -0,0 +1,25 @@
+// let word="this is the HTML templet";
+// let arr=['srinivas','banoth','syna','ammu','anameeka','srinu','srinivas','banoth','syna','ammu','anameeka','srinu','srinivas','banoth','syna','ammu','anameeka','srinu','srinivas','banoth','syna','ammu','anameeka','srinu','srinivas','banoth','syna','ammu','anameeka','srinu','srinivas','banoth','syna','ammu','anameeka','srinu','srinivas','banoth','syna','ammu','anameeka','srinu','srinivas','banoth','syna','ammu','anameeka','srinu'];
+
+// let html=`<h1>${word}</h1><ol>`
+// for(let i of arr){
+//     html=html+`<li>${i}</li>`
+// }
+// html=html+`</ol>`;
+
+// document.getElementById("demo").innerHTML=html;
+
+//INFINITY 
+
+// let number=2;
+// text="";
+// while(number!=Infinity){
+//    number= number+number;
+//     text=text+number+"<br>";
+
+// }
+// document.getElementById("demo").innerHTML=text;
+
+// THE MAXIMUM SAFE INTEGER 
+let x=Number.MAX_SAFE_INTEGER;
+document.getElementById('demo').innerHTML=x;
