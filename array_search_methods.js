f93a10aa5aaa14dac7801e92660317ec21557166
SRINIVAS BANOTH <145092139+srinu54321@users.noreply.github.com>
2024-01-02 07:17:09 +0530

    Add files via upload

---
+++ array_search_methods.js
@@ -0,0 +1,49 @@
+// let ar=[1,2,3,4,5,10,15,20,25];
+// function myfunction(value){
+//     return value>18;
+// }
+// let fun =ar.find(myfunction);
+// document.getElementById("demo").innerHTML =fun;
+// document.getElementById("demo").innerHTML=fun;
+
+//SORTING THE ARRAY 
+
+// const fruits= ['banana','apple','rose','mango','orange'];
+// let s=fruits.sort()
+// document.getElementById('demo4').innerHTML="s="+s;
+
+// let  frutits = ["Banana", "Orange", "Apple", "Mango"];
+// document.getElementById("demo1").innerHTML = frutits;
+
+// fruits.sort();
+// document.getElementById("demo2").innerHTML = frutits;
+
+// fruits.sort();
+// document.getElementById('demo3').innerHTML=frutits;
+
+//SORTING PROCESS IN JAVASCRIPT 
+
+// const ar=['srinivas','banoth','ammu','ameena','anameka','seena','syna'];
+// console.log(ar);
+// let sorting=ar.sort();
+// console.log(sorting);
+// console.log(ar.reverse())
+
+//DEFFARENT TECHNIQUE OF SORTING PROCESS 
+
+// const points = [40, 100, 1, 5, 25, 10];
+// document.getElementById("demo1").innerHTML=points;
+// points.sort(function(a,b){
+//     return a-b;
+// });
+// document.getElementById("demo2").innerHTML=points;
+
+// const points = [40, 100, 1, 5, 25, 10];
+// document.getElementById("demo1").innerHTML=points;
+// points.sort(function(a,b){
+//     return b-a;
+// });
+// document.getElementById("demo2").innerHTML=points;
+
+
+
